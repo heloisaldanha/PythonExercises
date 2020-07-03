0b98ddb0bac16438b9c4afac9b1282a2e8599b9c
Heloísa Saldanha <65875963+heloisaldanha@users.noreply.github.com>
2020-07-03 16:39:51 -0300

    Update and rename CursoEmVideo/Mundo_2/Aula #13 - Repetições/aula013_exe#48.py to CursoEmVideo/Mundo_2/Aula # 13 - Repetições/aula013_exe#48.py

--- CursoEmVideo/Mundo_2/Aula # 13 - Repetições/aula013_exe#48.py
+++ CursoEmVideo/Mundo_2/Aula # 13 - Repetições/aula013_exe#48.py
@@ -1,7 +1,7 @@
 # pegar todos os números múltiplos de 3 de 1 até 500 e somar todos
 soma = 0 # coloca o 0 para definr a variável (acumulador)
 cont = 0 # contador
-for contagem in range (1, 501, 2):
+for contagem in range (1, 501):
     if contagem % 3 == 0:
         cont = cont + 1 # um contador, geralmente conta mais um
         soma = soma + contagem # acumulador acumula valores (soma, multiplicação, etc)
