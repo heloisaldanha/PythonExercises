b740419856305f2d5921492a557da9e96e421683
Heloísa Saldanha <65875963+heloisaldanha@users.noreply.github.com>
2020-09-03 23:40:50 -0300

    Exercises

--- CursoEmVideo/Mundo_3/Aula # 18 - Listas pt 2/aula18_exe#86.py
+++ CursoEmVideo/Mundo_3/Aula # 18 - Listas pt 2/aula18_exe#86.py
@@ -2,3 +2,12 @@
 crie um programa que crie uma matriz de dimensão 3x3 e a preencha com valores lidos pelo teclado.
 no final, mostre a matriz na tela, com a formatação correta.
 '''
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for linha in range(0, 3):  # colocar os valores dentro da matriz
+    for coluna in range(0, 3):
+        matriz[linha][coluna] = int(input('Digite um valor para inserir no lugar [{}, {}]: '.format(linha, coluna)))
+for linha in range(0,3):  # para mostrar a estrutura na tela
+    for coluna in range(0, 3):
+        print('[{:^5}]'.format(matriz[linha][coluna]), end='')
+    print()
