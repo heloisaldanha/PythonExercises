b60a160593e13f3483e2b4683a093b0101ae6d70
Heloísa Saldanha <65875963+heloisaldanha@users.noreply.github.com>
2020-09-03 23:44:16 -0300

    Exercises

--- CursoEmVideo/Mundo_3/Aula # 18 - Listas pt 2/aula18_exe#88.py
+++ CursoEmVideo/Mundo_3/Aula # 18 - Listas pt 2/aula18_exe#88.py
@@ -3,3 +3,27 @@ faca um programa qie ajude um jogador da mega sena a criar palpites. o programa 
 quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando
 tudo em uma lista composta.
 '''
+# não consegui fazer :(
+
+from random import randint
+lista = []  #
+jogos = []
+quantosJogos = int(input('Quantos jogos você deseja sortear? '))
+totalJogos = 1  # o total começa com um pra mostrar no print depois, sem precisar somar +1
+while totalJogos <= quantosJogos:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1  # esse contador aqui serve pra contar os 6 números de sorteio
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])  # vai copiar várias listas dentro de uma lista grande
+    lista.clear()
+    totalJogos += 1
+print('Sorteando {} jogos'.format(quantosJogos))
+for i, l in enumerate(jogos):
+# i é a quantidade de voltas que o for deu, l é cada lista menor dentro de jogos
+    print('Jogo {}: {}'.format(i + 1, l))
