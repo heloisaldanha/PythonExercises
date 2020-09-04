9c087e17b656d444ea3ebed8ca37cfbee7fa3c25
Heloísa Saldanha <65875963+heloisaldanha@users.noreply.github.com>
2020-09-03 23:43:29 -0300

    Exercises

--- CursoEmVideo/Mundo_3/Aula # 18 - Listas pt 2/aula18_exe#87.py
+++ CursoEmVideo/Mundo_3/Aula # 18 - Listas pt 2/aula18_exe#87.py
@@ -4,3 +4,24 @@ a) a soma de todos os valores pares digtados.
 b) a soma dos valores da terceira coluna
 c) o maior valor da segunda linha.
 '''
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+somaPar = maior = somaColuna = 0
+for linha in range(0, 3):  # colocar os valores dentro da matriz
+    for coluna in range(0, 3):
+        matriz[linha][coluna] = int(input('Digite um valor para inserir no lugar [{}, {}]: '.format(linha, coluna)))
+for linha in range(0,3):  # para mostrar a estrutura na tela
+    for coluna in range(0, 3):
+        print('[{:^5}]'.format(matriz[linha][coluna]), end='')
+        if matriz[linha][coluna] % 2 == 0:
+            somaPar += matriz[linha][coluna]
+    print()
+print()
+for linha in range(0, 3):  # fazer o for pra linha, porque a coluna é fixa
+    somaColuna += matriz[linha][2]  # o [2] é a coluna fixa
+for coluna in range(0, 3):
+    if matriz[1][coluna] > maior:
+        maior = matriz[1][coluna]
+print(somaPar)
+print(somaColuna)
+print(maior)
